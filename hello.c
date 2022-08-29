f2a092ceb2435ee16df6e0128d19c484b5340b18
Nurul Islam <nurulbdpersonal@gmail.com>
2022-08-29 19:30:57 +0600

    first commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello World\n");
+    return 0;
+}
+
+
+
+// PS C:\programming> gcc
+// gcc.exe: fatal error: no input files
+// compilation terminated.
+// PS C:\programming> gcc hello.c
+// PS C:\programming> ./a.exe
+// Hello World
+// PS C:\programming>
